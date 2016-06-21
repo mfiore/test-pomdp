58d14abf0673c32594d9a8b8615dda3fe9053d73
Michelangelo Fiore <mfiore@bagatelle.laas.fr>
2016-06-21 18:37:32 +0200

    a

--- newmain.cpp
+++ newmain.cpp
@@ -40,14 +40,24 @@ using namespace std;
 int main(int argc, char** argv) {
     cout << "Prova\n";
 
-    map<string, Hmdp*> hmdp_map;
-    hmdp_map["assemble"] = new AssembleBracket();
-    hmdp_map["clean"] = new CleanSurface();
-    hmdp_map["glue"] = new GlueSurface();
-    hmdp_map["get"] = new GetObject();
-    hmdp_map["apply"] = new AttachBracket();
-    hmdp_map["wait"] = new Wait();
-    MmdpManager manager(hmdp_map);
+    map<string, Hmdp*> hmdp_map_1, hmdp_map_2;
+    hmdp_map_1["assemble"] = new AssembleBracket();
+    hmdp_map_1["clean"] = new CleanSurface();
+    hmdp_map_1["glue"] = new GlueSurface();
+    hmdp_map_1["get"] = new GetObject();
+    hmdp_map_1["apply"] = new AttachBracket();
+    hmdp_map_1["wait"] = new Wait();
+
+    hmdp_map_2["assemble"] = new AssembleBracket();
+    hmdp_map_2["clean"] = new CleanSurface();
+    hmdp_map_2["glue"] = new GlueSurface();
+    hmdp_map_2["get"] = new GetObject();
+    hmdp_map_2["apply"] = new AttachBracket();
+    hmdp_map_2["wait"] = new Wait();
+
+    vector<map<string, Hmdp* > > v_hmdp{hmdp_map_1, hmdp_map_2};
+    MmdpManager manager(v_hmdp);
+
 
 
     Mmdp saphari_mmdp(&manager);
@@ -76,7 +86,7 @@ int main(int argc, char** argv) {
     inital_set["bracket2_isAt"] = "table";
     inital_set["bracket3_isAt"] = "table";
     inital_set["gluebottle_isAt"] = "table";
-    inital_set["surface1_status"] = "cleaned";
+    inital_set["surface1_status"] = "none";
     inital_set["surface2_status"] = "none";
     inital_set["surface3_status"] = "none";
 
@@ -86,25 +96,32 @@ int main(int argc, char** argv) {
 
     saphari_mmdp.printQValues(initial_state);
     //    saphari_mmdp.printRewardFunction();
-    //    PairStateAction p;
+        PairStateAction p;
     VariableSet para = saphari_mmdp.convertToParametrizedState(initial_state);
 
     int i = saphari_mmdp.mapStateEnum[para];
-    //    
-    //    p.first = saphari_mmdp.mapStateEnum.at(para);
-    //    p.second = "agentp0_move_surfacep0-agentp1_wait";
-    ////    pair<vector<string>, set<string> > r=saphari_mmdp.getSubMdpName(p.second);
-    ////    Hmdp* h=saphari_mmdp.hierarchy_map_[r.first[0]];
-    // 
-    ////    map<VariableSet,double> stateprob = h->getHierarchicTransition(initial_state,&saphari_mmdp);
-    //    StateProb stateprob=saphari_mmdp.transition[p];
+
+    p.first = saphari_mmdp.mapStateEnum.at(para);
+    p.second = "agentp0_assemble_bracket1_surface1-agentp1_wait";
+    pair<vector<string>, set<string> > r = saphari_mmdp.getSubMdpName(p.second);
+    Mmdp* h =(Mmdp*) saphari_mmdp.hierarchy_map_[r.first[0]];
+    
+    set<string> changed_mdps {"agent0","agent1"};
+    
+    map<string,string> super_instance;
+    super_instance["agentp0"]="agent1";
+    super_instance["agentp1"]="agent2";
+    h->assignParametersFromActionName("agent1_assemble_bracket1_surface1-agent2_wait",changed_mdps,super_instance);
+//    // 
+    map<VariableSet, double> stateprob = h->getHierarchicTransition(initial_state, &saphari_mmdp);
+    //        StateProb stateprob=saphari_mmdp.transition[p];
     //
-    //    for (auto s : stateprob) {
-    ////        VariableSet v=s.first;
-    //     
-    //        VariableSet v=saphari_mmdp.vecStateEnum[s.first];
-    //        cout<<v.toString();
-    //    }
+    for (auto s : stateprob) {
+        VariableSet v = s.first;
+        //     
+//        VariableSet v = saphari_mmdp.vecStateEnum[s.first];
+        cout << v.toString();
+    }
     //    saphari_mmdp.enumerateFunctions("prova.pomdp");
     //    saphari_mmdp.printTransitionFunction();
     saphari_mmdp.simulate(15, initial_state);
